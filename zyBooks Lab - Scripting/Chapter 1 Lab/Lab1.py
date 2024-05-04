6576dfd816aba0f314e25bb6d0caf61ec59842d8
Daniel <thirday9@gmail.com>
2024-05-04 15:26:47 -0400

    First Lab

---
+++ zyBooks Lab - Scripting/Chapter 1 Lab/Lab1.py
@@ -0,0 +1,14 @@
+#Write a program using integers user_num and x as input, and output user_num divided by x three times
+
+#Note: In Python 3, integer division discards fractions. Ex: 6 // 4 is 1 (the 0.5 is discarded).
+
+user_num = int(input('Enter number:')\n)
+
+divide_num = int(input('Enter dividing number:')\n)
+
+#This will divide the numbers three times
+first = user_num // divide_num
+second = first // divide_num
+third = second // divide_num
+
+print(first, second, third)
