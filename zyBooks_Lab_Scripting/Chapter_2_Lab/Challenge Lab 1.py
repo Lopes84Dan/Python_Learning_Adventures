610085891dde765dba8da7e960f6481fb534ab30
Daniel <thirday9@gmail.com>
2024-05-06 16:25:45 -0400

    PyCharm

---
+++ zyBooks_Lab_Scripting/Chapter_2_Lab/Challenge Lab 1.py
@@ -0,0 +1,11 @@
+# First, write two input() statements to read in values for my_city followed by my_state.
+# Then, use string concatenation to assign log_entry with current_time, my_city, and my_state.
+# Separate each value with a space character (' ').
+
+current_time = '2020-07-26 02:12:18:'
+my_city = input('Enter city:\n')
+my_state = input('Enter city:\n')
+
+log_entry = current_time + ' ', my_city + ' ' + my_state
+
+print(log_entry)
